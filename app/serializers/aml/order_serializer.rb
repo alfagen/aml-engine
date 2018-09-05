4b00306e08fdc630d782397cc163a475b86020ed
Danil Pismenny <danil@brandymint.ru>
2018-09-05 16:32:37 +0300

    Сериализация Order#is_locked

--- app/serializers/aml/order_serializer.rb
+++ app/serializers/aml/order_serializer.rb
@@ -7,5 +7,9 @@ module AML
     has_many :order_documents, record_type: :aml_order_document
 
     attributes :first_name, :surname, :patronymic, :workflow_state, :birth_date
+
+    attribute :is_locked do |o|
+      o.is_locked?
+    end
   end
 end

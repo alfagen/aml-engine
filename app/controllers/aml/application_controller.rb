d119cbb22d8bfab4d442f06da179aa480b85538e
Danil Pismenny <danil@brandymint.ru>
2018-09-09 21:36:41 +0300

    AML::ApplicationController наследуется от ::ApplicationController

--- app/controllers/aml/application_controller.rb
+++ app/controllers/aml/application_controller.rb
@@ -1,18 +1,4 @@
 module AML
-  class ApplicationController < ActionController::Base
-    before_action :require_login
-
-    rescue_from ActionController::InvalidAuthenticityToken, with: :rescue_invalid_authenticity_token
-
-    private
-
-    def not_authenticated
-      render 'aml/application/not_authenticated', layout: 'simple'
-    end
-
-    def rescue_invalid_authenticity_token
-      flash.alert = 'Просрочен токен аутентификации, авторизуйтесь снова'
-      render 'aml/application/not_authenticated', layout: 'simple'
-    end
+  class ApplicationController < ::ApplicationController
   end
 end

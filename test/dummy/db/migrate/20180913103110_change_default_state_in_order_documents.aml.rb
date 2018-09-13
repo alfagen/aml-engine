0f6812330b871ddc8cd3b646401ea19b476c0b68
ValentinAndreev <valentinandreev80@gmail.com>
2018-09-13 14:15:09 +0300

    fixes

--- test/dummy/db/migrate/20180913103110_change_default_state_in_order_documents.aml.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from aml (originally 20180911074310)
-class ChangeDefaultStateInOrderDocuments < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default :aml_order_documents, :workflow_state, 'none'
-  end
-end
--- test/dummy/db/migrate/20180913103112_rename_table.aml.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from aml (originally 20180913100117)
-class RenameTable < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :aml_client_document_fields, :aml_document_fields
-  end
-end

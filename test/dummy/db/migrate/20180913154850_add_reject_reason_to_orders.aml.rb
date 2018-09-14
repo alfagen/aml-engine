9fae781f1421e839f6cfee5e92a618fcde7076b3
ValentinAndreev <valentinandreev80@gmail.com>
2018-09-14 11:39:45 +0300

    fix

--- test/dummy/db/migrate/20180913154850_add_reject_reason_to_orders.aml.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from aml (originally 20180911080249)
-class AddRejectReasonToOrders < ActiveRecord::Migration[5.2]
-  def change
-    add_column :aml_orders, :reject_reason, :text
-  end
-end

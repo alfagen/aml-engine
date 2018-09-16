928a1f19ff0cd2c103f072a30efd22399eafd67b
ValentinAndreev <valentinandreev80@gmail.com>
2018-09-16 16:39:30 +0300

    подключние url хелперов к engine

--- lib/aml/engine.rb
+++ lib/aml/engine.rb
@@ -2,6 +2,10 @@ module AML
   class Engine < ::Rails::Engine
     isolate_namespace AML
 
-    # см. - https://guides.rubyonrails.org/engines.html
+    initializer 'aml.url_helpers' do
+      ActiveSupport.on_load(:action_controller) do
+        helper Rails.application.routes.url_helpers
+      end
+    end
   end
 end

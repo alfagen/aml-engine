45b2a0599ac9e768e48a4ec43ee2fef2a98eecb5
ValentinAndreev <valentinandreev80@gmail.com>
2018-09-17 10:20:31 +0300

    remove helpers from engine.rb

--- lib/aml/engine.rb
+++ lib/aml/engine.rb
@@ -1,11 +1,5 @@
 module AML
   class Engine < ::Rails::Engine
     isolate_namespace AML
-
-    initializer 'aml.url_helpers' do
-      ActiveSupport.on_load(:action_controller) do
-        helper Rails.application.routes.url_helpers
-      end
-    end
   end
 end

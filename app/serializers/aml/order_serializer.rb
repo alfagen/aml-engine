810934ea5ef584d567e6b3491a1de68f0058a65d
Danil Pismenny <danil@brandymint.ru>
2018-09-25 11:46:42 +0300

    Добавил статус в сериализацию заявки

--- app/serializers/aml/order_serializer.rb
+++ app/serializers/aml/order_serializer.rb
@@ -5,6 +5,7 @@ module AML
     set_type :aml_order
 
     has_many :order_documents, record_type: :aml_order_document
+    belongs_to :aml_status, record_type: :aml_status, serializer: 'AML::StatusSerializer'
 
     attributes :first_name, :surname, :patronymic, :workflow_state, :birth_date
 

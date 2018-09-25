29522361ce1dca7b0b13a4f4d8132c6671ded104
Danil Pismenny <danil@brandymint.ru>
2018-09-25 10:43:52 +0300

    У заявки статус обязателен

--- db/migrate/20180924130455_add_status_to_order.rb
+++ db/migrate/20180924130455_add_status_to_order.rb
@@ -1,5 +1,7 @@
 class AddStatusToOrder < ActiveRecord::Migration[5.2]
   def change
     add_reference :aml_orders, :aml_status, foreign_key: true
+
+    AML::Order.update_all aml_status: AML.default_status if AML::Status.any?
   end
 end

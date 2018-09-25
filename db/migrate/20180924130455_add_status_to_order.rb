da5d78a10eb0ce17f0d11ed942f71f486a0c324b
Danil Pismenny <danil@brandymint.ru>
2018-09-25 11:56:35 +0300

    fix migration

--- db/migrate/20180924130455_add_status_to_order.rb
+++ db/migrate/20180924130455_add_status_to_order.rb
@@ -2,6 +2,6 @@ class AddStatusToOrder < ActiveRecord::Migration[5.2]
   def change
     add_reference :aml_orders, :aml_status, foreign_key: true
 
-    AML::Order.update_all aml_status: AML.default_status if AML::Status.any?
+    AML::Order.update_all aml_status_id: AML.default_status.id if AML::Status.any?
   end
 end

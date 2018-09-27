a406c99163fada8f520bd01811e1f8d967089d79
Danil Pismenny <danil@brandymint.ru>
2018-09-27 13:29:52 +0300

    Переименовал секвенции в факторисах чтобы не мешались в основнов приложении

--- factories/aml_status.rb
+++ factories/aml_status.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
-  sequence(:title) { |n| "title#{n}" }
-  sequence(:key) { |n| "key#{n}" }
+  sequence(:aml_status_title) { |n| "title#{n}" }
+  sequence(:aml_status_key) { |n| "key#{n}" }
   factory(:aml_status, class: AML::Status) do
-    title { generate :title }
-    key { generate :key }
+    title { generate :aml_status_title }
+    key { generate :aml_status_key }
 
     trait :default do
       key { AML.default_status_key }

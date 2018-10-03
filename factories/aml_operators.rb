19082a92f9fea4de05da9c488bb2640aca98cfcf
Danil Pismenny <danil@brandymint.ru>
2018-10-03 14:41:52 +0300

    Добавит trait :administrator в factory opertor

--- factories/aml_operators.rb
+++ factories/aml_operators.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
   factory(:aml_operator, class: AML::Operator) do
     sequence(:email) { |n| "user#{n}@example.com" }
     sequence(:name) { |n| "operator#{n}" }
+
+    trait :administrator do
+      role { 'administrator' }
+    end
   end
 end

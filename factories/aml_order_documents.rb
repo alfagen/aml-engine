cb5e5b02d8674a49da652aa4afbc6f7758312d42
Valentin Andreev <valentinandreev80@gmail.com>
2018-10-08 10:43:37 +0300

    loaded

--- factories/aml_order_documents.rb
+++ factories/aml_order_documents.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory(:aml_order_document, class: AML::OrderDocument) do
     association :order, factory: :aml_order
     association :document_kind, factory: :aml_document_kind
+
+    trait :loaded do
+      image { Rack::Test::UploadedFile.new(Rails.root.join('test_files', 'test.png')) }
+    end
   end
 end

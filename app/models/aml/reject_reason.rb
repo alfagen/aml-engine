6d980dbf694dd1b39b3792c5cd4c339365feb5d6
Valentin Andreev <valentinandreev80@gmail.com>
2018-10-10 11:44:57 +0300

    document-reject-reason

--- app/models/aml/reject_reason.rb
+++ app/models/aml/reject_reason.rb
@@ -3,7 +3,10 @@ module AML
     include Archivable
     scope :ordered, -> { order :id }
 
+    enum kind: ['order_reason', 'order_document_reason']
+
     has_many :orders, foreign_key: :aml_reject_reason_id
+    has_many :order_documents, foreign_key: :aml_reject_reason_id
 
     validates :title, presence: true, uniqueness: true
   end

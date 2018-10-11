512f91dd663ab0baf928605b9dcb9d7c44415c08
Danil Pismenny <danil@brandymint.ru>
2018-10-11 20:14:47 +0300

    Добавил reject_reason в сериализацию order_document

--- app/serializers/aml/order_document_serializer.rb
+++ app/serializers/aml/order_document_serializer.rb
@@ -5,9 +5,16 @@ class AML::OrderDocumentSerializer
 
   belongs_to :order, record_type: :aml_order
   belongs_to :document_kind, record_type: :aml_document_kind
+  belongs_to :aml_reject_reason, record_type: :aml_status, serializer: 'AML::RejectReasonSerializer'
+
   has_many :document_fields, record_type: :aml_document_fields
 
   attributes :order_id, :image,
     :document_fields_attributes, :fields,
-    :workflow_state
+    :workflow_state,
+    :reject_reason_details
+
+  attribute :reject_reason do |od|
+    [od.aml_reject_reason.title, od.reject_reason_details].compact.join('. ') if od.rejected?
+  end
 end

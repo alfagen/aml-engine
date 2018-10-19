19a4c482c5316449cb96283680c265f6b8c55675
ValentinAndreev <valentinandreev80@gmail.com>
2018-10-19 14:09:08 +0300

    migration fix

--- db/migrate/20181019065150_add_operator_locale.rb
+++ db/migrate/20181019065150_add_operator_locale.rb
@@ -1,6 +1,7 @@
 class AddOperatorLocale < ActiveRecord::Migration[5.2]
   def change
-    add_column :aml_operators, :locale, :string, default: 'ru', null: false
+    add_column :aml_operators, :locale, :string, default: 'ru'
     execute "update aml_operators set locale='ru'"
+    change_column_null :aml_operators, :locale, false
   end
 end

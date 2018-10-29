89322afb8f909076ab8764a53fb5061e17b8d574
Valentin Andreev <valentinandreev80@gmail.com>
2018-10-29 22:41:37 +0300

    remove sorcery columns from operator model

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_101822) do
+ActiveRecord::Schema.define(version: 2018_10_29_155905) do
 
   create_table "aml_clients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
@@ -110,22 +110,13 @@ ActiveRecord::Schema.define(version: 2018_10_26_101822) do
   end
 
   create_table "aml_operators", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "crypted_password"
-    t.string "salt"
     t.string "workflow_state", default: "unblocked", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_token_expires_at"
-    t.datetime "reset_password_email_sent_at"
-    t.integer "access_count_to_reset_password_page", default: 0
     t.integer "role", default: 0, null: false
     t.string "name", null: false
     t.string "locale", default: "ru", null: false
     t.string "time_zone_name"
-    t.index ["email"], name: "index_aml_operators_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_aml_operators_on_reset_password_token"
   end
 
   create_table "aml_order_documents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

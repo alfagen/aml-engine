d0ac2aebc6c1a1070287eeeb6958c6923ace4eb7
Danil Pismenny <danil@brandymint.ru>
2018-10-29 18:13:28 +0300

    [#161381282] При сбросе клиента сбрасывается счетчик операций

--- app/models/aml/client.rb
+++ app/models/aml/client.rb
@@ -37,7 +37,7 @@ module AML
 
     def reset_status!
       with_lock do
-        update aml_status: nil, aml_accepted_order: nil
+        update aml_status: nil, aml_accepted_order: nil, total_operations_count: 0, total_income_amount: Money.new(0, Money.default_currency)
         create_current_order!
       end
     end

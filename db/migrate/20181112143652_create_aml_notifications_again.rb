d9d7aa7091c6aa855ea6624f6c86aa9d5615a80f
Danil Pismenny <danil@brandymint.ru>
2018-11-13 15:20:47 +0300

    fixed migration

--- db/migrate/20181112143652_create_aml_notifications_again.rb
+++ db/migrate/20181112143652_create_aml_notifications_again.rb
@@ -1,3 +1,4 @@
+# This migration comes from aml (originally 20181112143652)
 class CreateAMLNotificationsAgain < ActiveRecord::Migration[5.2]
   def change
     create_table :aml_notifications do |t|
@@ -10,16 +11,28 @@ class CreateAMLNotificationsAgain < ActiveRecord::Migration[5.2]
 
     add_reference :aml_notification_templates, :aml_notification
 
-    AML::NotificationTemplate.find_each do |nt|
-      n = AML::Notification.find_or_create_by(title: nt.key)
-      nt.update aml_notification: n
-    end
+    remove_index :aml_notification_templates, name: :index_aml_notification_templates_on_key_and_locale
 
-    change_column_default :aml_notification_templates, :aml_notification_id, false
+    AML::Notification.reset_column_information
+    AML::NotificationTemplate.reset_column_information
+    change_column_null :aml_notification_templates, :key, true
+    migrate_notifications
     remove_column :aml_notification_templates, :key
 
+    change_column_default :aml_notification_templates, :aml_notification_id, false
 
-    remove_index :aml_notification_templates, :locale
     add_index :aml_notification_templates, [:aml_notification_id, :locale], unique: true, name: :aml_notification_templates_uniq
+  end
+
+  private
+
+  def migrate_notifications
+    AML::Notification.delete_all
+    AML::NotificationTemplate.find_each do |nt|
+      if nt.key.present?
+        n = AML::Notification.find_or_create_by(title: nt.key)
+        nt.update_column :aml_notification_id, n
+      end
+    end
   end
 end

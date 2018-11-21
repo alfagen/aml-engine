f0af3dc04a55538f81bc73b82a358974a589c5e4
Danil Pismenny <danil@brandymint.ru>
2018-11-21 09:04:15 +0300

    ClientInfo доступна всем

--- app/authorizers/aml/client_info_authorizer.rb
+++ app/authorizers/aml/client_info_authorizer.rb
@@ -1,23 +1,7 @@
 module AML
   class ClientInfoAuthorizer < ApplicationAuthorizer
-    def self.updatable_by?(_operator)
+    def self.default(_adjective, _operator)
       true
-    end
-
-    def updatable_by?(_operator)
-      true
-    end
-
-    def self.readable_by?(_operator)
-      true
-    end
-
-    def readable_by?(_operator)
-      true
-    end
-
-    def resetable_by?(operator)
-      updatable_by? operator
     end
   end
 end

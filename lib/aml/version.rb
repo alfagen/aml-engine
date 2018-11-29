f5cb414627852a3a79e2065d62f4fdcdbd33ccc9
Valentin Andreev <valentinandreev80@gmail.com>
2018-11-29 18:36:52 +0300

    версия из .semver

--- lib/aml/version.rb
+++ lib/aml/version.rb
@@ -1,3 +1,9 @@
 module AML
-  VERSION = '0.4.6'
+  AML::VERSION = version
+
+  def version
+    File.read('.semver').split("\n")[1..3].map{ |x| x.split(' ')[1] }.join('.')
+  rescue
+    raise 'File .semver not exists or not valid.'
+  end
 end

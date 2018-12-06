0dedebf4733f6e3de64239bc9379d095ec7bd2fb
Valentin Andreev <valentinandreev80@gmail.com>
2018-12-06 20:31:18 +0300

    причины отказа для платежных карт

--- app/models/aml/reject_reason.rb
+++ app/models/aml/reject_reason.rb
@@ -9,12 +9,13 @@ module AML
     scope :ordered, -> { order :id }
 
     has_many :orders, foreign_key: :aml_reject_reason_id
+    has_many :card_bindings, foreign_key: :aml_reject_reason_id
     has_many :order_documents, foreign_key: :aml_reject_reason_id
 
     validates :title, presence: true, uniqueness: true
 
     validates :kind, presence: true
 
-    enum kind: ['order_reason', 'order_document_reason']
+    enum kind: ['order_reason', 'order_document_reason', 'card_binding_reason']
   end
 end

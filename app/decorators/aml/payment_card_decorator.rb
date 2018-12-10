8b946e7a1810f45d92008b7da2b3495d4aac495f
ValentinAndreev <valentinandreev80@gmail.com>
2018-12-10 10:12:48 +0300

    поправка декораторов

---
+++ app/decorators/aml/payment_card_decorator.rb
@@ -0,0 +1,26 @@
+require_relative 'application_decorator'
+
+module AML
+  class PaymentCardDecorator < ApplicationDecorator
+    delegate_all
+
+    def reject_reason
+      return unless object.aml_reject_reason.present?
+
+      buffer = []
+      buffer << object.aml_reject_reason.title
+      buffer << h.content_tag(:div, object.reject_reason_details) if object.reject_reason_details.present?
+
+      h.content_tag :div, buffer.join.html_safe, class: 'alert alert-info'
+    end
+
+    # Define presentation-specific methods here. Helpers are accessed through
+    # `helpers` (aka `h`). You can override attributes, for example:
+    #
+    #   def created_at
+    #     helpers.content_tag :span, class: 'time' do
+    #       object.created_at.strftime("%a %m/%d/%y")
+    #     end
+    #   end
+  end
+end

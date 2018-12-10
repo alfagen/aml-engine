fe85571a06686727831fe0cc1afdb84edcb2a340
Valentin Andreev <valentinandreev80@gmail.com>
2018-12-10 10:06:28 +0300

    поправки (авторизатор, ссылка на клиента, пагинация) (#124)

--- app/authorizers/aml/payment_card_order_authorizer.rb
+++ app/authorizers/aml/payment_card_order_authorizer.rb
@@ -1,25 +1,15 @@
 module AML
   class PaymentCardOrderAuthorizer < ApplicationAuthorizer
-    EVENTS = %i[accept reject].freeze
-    OWNER_REQUIRED_FOR_EVENTS = %i[accept reject].freeze
-
     def self.readable_by?(_user)
       true
     end
 
-    EVENTS.each do |event|
-      ability = Authority.abilities[event] || raise("No ability for event #{event}")
-      define_singleton_method "#{ability}_by?" do |_operator|
-        true
-      end
+    def acceptable_by?(operator)
+      operator.administrator?&& resource.can_accept?
     end
 
-    EVENTS.each do |event|
-      ability = Authority.abilities[event] || raise("No ability for event #{event}")
-      define_method "#{ability}_by?" do |operator|
-        resource.enabled_workflow_events.include?(event) \
-          && (OWNER_REQUIRED_FOR_EVENTS.exclude?(event) || operator.administrator?)
-      end
+    def rejectable_by?(operator)
+      operator.administrator? && resource.can_reject?
     end
   end
 end

e5ad724fa51fff58654e894b5ca3e0294c172984
ValentinAndreev <valentinandreev80@gmail.com>
2018-12-11 15:57:58 +0300

    оператор обрабатывает заявки на карты

--- app/authorizers/aml/payment_card_order_authorizer.rb
+++ app/authorizers/aml/payment_card_order_authorizer.rb
@@ -1,23 +1,4 @@
 module AML
-  class PaymentCardOrderAuthorizer < ApplicationAuthorizer
-    EVENTS = %i[done start accept reject cancel].freeze
-
-    def self.readable_by?(user)
-      user.administrator?
-    end
-
-    EVENTS.each do |event|
-      ability = Authority.abilities[event] || raise("No ability for event #{event}")
-      define_singleton_method "#{ability}_by?" do |_operator|
-        true
-      end
-    end
-
-    EVENTS.each do |event|
-      ability = Authority.abilities[event] || raise("No ability for event #{event}")
-      define_method "#{ability}_by?" do |operator|
-        resource.enabled_workflow_events.include?(event) && operator.administrator?
-      end
-    end
+  class PaymentCardOrderAuthorizer < OrderAuthorizer
   end
 end

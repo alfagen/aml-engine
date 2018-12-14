41e7606fa97048f1ca943406e0cfb985cf6f33b6
Valentin Andreev <valentinandreev80@gmail.com>
2018-12-14 10:54:49 +0300

    concerns для notification и  workflow

--- app/models/aml/payment_card_order.rb
+++ app/models/aml/payment_card_order.rb
@@ -32,12 +32,6 @@ module AML
       allow_done?
     end
 
-    def reject(reject_reason:, details: nil)
-      halt! 'Причина должна быть указана' unless reject_reason.is_a? AML::RejectReason
-      update aml_reject_reason: reject_reason, reject_reason_details: details
-      touch :operated_at
-    end
-
     def accepted_at
       return operated_at if accepted?
     end
@@ -55,15 +49,6 @@ module AML
 
     def is_owner?(operator)
       self.operator == operator
-    end
-
-    def start(operator:)
-      update operator: operator
-    end
-
-    def cancel
-      update operator: nil
-      touch :operated_at
     end
 
     def done

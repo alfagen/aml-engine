ffe3b7832e7ce5329ae859591517e40d5dab70a3
Danil Pismenny <danil@brandymint.ru>
2018-12-18 00:28:17 +0300

    Удалил не используемые ассоциации из AML::Notification

--- app/models/aml/notification.rb
+++ app/models/aml/notification.rb
@@ -4,10 +4,6 @@ module AML
 
     has_many :aml_notification_templates, class_name: 'AML::NotificationTemplate', foreign_key: :aml_notification_id, dependent: :destroy
 
-    has_one :status_on_pending_notification, class_name: 'AML::Status', foreign_key: :on_pending_notification_id, dependent: :nullify
-    has_one :status_on_accept_notification, class_name: 'AML::Status', foreign_key: :on_accept_notification_id, dependent: :nullify
-    has_one :status_on_reject_notification, class_name: 'AML::Status', foreign_key: :on_reject_notification_id, dependent: :nullify
-
     validates :title, presence: true, uniqueness: true
 
     after_create :create_templates

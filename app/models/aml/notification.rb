625f21e55b5a43b1da1883fb7ad79bf7ea4eacb9
ValentinAndreev <valentinandreev80@gmail.com>
2018-12-18 12:06:16 +0300

    добавлен key для причины уведомления

--- app/models/aml/notification.rb
+++ app/models/aml/notification.rb
@@ -1,12 +1,16 @@
 module AML
   class Notification < ApplicationRecord
+    extend Enumerize
     include Authority::Abilities
 
     has_many :aml_notification_templates, class_name: 'AML::NotificationTemplate', foreign_key: :aml_notification_id, dependent: :destroy
 
     validates :title, presence: true, uniqueness: true
+    validates :key, uniqueness: true, allow_blank: true
 
     after_create :create_templates
+
+    enumerize :key, in: %w(payment_card_pending payment_card_accepted payment_card_rejected)
 
     def to_s
       title
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_081652) do
+ActiveRecord::Schema.define(version: 2018_12_18_072204) do
 
   create_table "aml_agreement_translations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "aml_agreement_id", null: false
@@ -190,6 +190,7 @@ ActiveRecord::Schema.define(version: 2018_12_14_081652) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "key"
     t.index ["title"], name: "index_aml_notifications_on_title", unique: true
   end
 

af95a864d5305f18b33d3f321ad32b1a6b6985d5
Danil Pismenny <danil@brandymint.ru>
2018-12-24 10:17:32 +0300

    [fixes #162809869] NameError in aml/payment_card_orders#create

--- app/models/concerns/aml/order_notifications.rb
+++ app/models/concerns/aml/order_notifications.rb
@@ -23,9 +23,13 @@ module AML
       end
 
       client.notify notification_template.template_id,
-        first_name: (first_name.presence || client.first_name),
+        first_name: client_first_name,
         reject_reason_title: aml_reject_reason.try(:title),
         reject_reason_details: reject_reason_details.presence
+    end
+
+    def client_first_name
+      read_attribute(:first_name).presence || client.first_name
     end
 
     def notification_locale

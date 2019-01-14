0fb04977b03a9e3775281f021c12aeb9ff822cd5
Danil Pismenny <danil@brandymint.ru>
2019-01-14 17:30:38 +0300

    [#163188018] fix Проблема с транзакционными письмами по верификации.

--- app/mailers/aml/application_mailer.rb
+++ app/mailers/aml/application_mailer.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module AML
-  class ApplicationMailer < ActionMailer::Base
-    # советуют ловить только ActiveJob::DeserializationError
-    # но мне интересно что там вообще может падать
-    rescue_from StandardError do |exception|
-      AML.logger.error exception
-      Bugsnag.notify exception if defined? Bugsnag
-      raise exception
-    end
+  class ApplicationMailer < ::ApplicationMailer
   end
 end

97cc1c31fd327d0213861ad6de1c1cf98fe785d2
Danil Pismenny <danil@brandymint.ru>
2025-10-19 19:33:42 +0300

    Revert "Remove Sorcery"
    This reverts commit 9298603c8d5708b2bf3bf1c4c420cf85e261cdaf.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,6 +46,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.include FactoryBot::Syntax::Methods
+  config.include Sorcery::TestHelpers::Rails::Controller, type: :controller
+  config.include Sorcery::TestHelpers::Rails::Integration, type: :feature
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.

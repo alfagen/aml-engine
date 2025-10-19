9b5c189f125b5a58e152398b203b7b4625fd0204
Danil Pismenny <danil@brandymint.ru>
2025-10-19 19:33:54 +0300

    Revert "Remove sorcery"
    This reverts commit c4aa3081cb649b6ee14b98a9a9d62ca48043a199.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -8,14 +8,13 @@ require 'active_model/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
-require 'action_mailer/railtie'
-require 'active_job/railtie'
 require 'rails/test_unit/railtie'
 require 'sprockets/railtie'
 
 Bundler.require(*Rails.groups)
 
 require 'archivable'
+require 'sorcery'
 require "aml"
 require 'authority'
 require 'carrierwave'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require 'archivable'
+require 'sorcery'
 require 'rspec/rails'
 require 'factory_bot'
 require 'pry'
